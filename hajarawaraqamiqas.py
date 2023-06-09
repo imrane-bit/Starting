09feced44d49ac687789dfd10528e1c3fd2e544b
imrane-bit <74026154+imrane-bit@users.noreply.github.com>
2023-06-09 13:40:00 +0100

    Update hajarawaraqamiqas.py
    this is a simple beginner-making rockcisorpaper game in morrocan darija

--- hajarawaraqamiqas.py
+++ hajarawaraqamiqas.py
@@ -9,41 +9,54 @@ import random
 
 #y = filter(lambda x :x%3==0,lis)
 #print (list(y))
-
+print
 
 
 
 
 def game():
-    props = [
+    lose = [
     "wrqa+mqs",
     "hjra+wrqa",
     "mqs+hjra"
     ]
     draws = ["hjra+hjra","wrqa+wrqa","mqs+mqs"]
+    #wins = ["wrqa+hjra","mqs+"]
     prop =["mqs","hjra","wrqa"]
     tries = 0
     loses = 0
     wins = 0
-    while tries <= 3:
+    eror = 0
+    while tries < 3:
+        play = input("khtar hjra/wrqa/mqs :")
         zhr = random.choice(prop)
-        play = input("khtar hjra/wrqa/mqs : ")
-        if str(play+"+"+zhr) in props :
+        
+        if str(play+"+"+zhr) in lose :
             print(str(zhr) + " ,lost!")
             tries += 1
             loses +=1
         elif str(play+"+"+zhr) in draws :
             print (str(zhr)+" ,ta3adol")
             tries += 1
-        else :
+        elif str(zhr+"+"+play) in lose :
             print(str(zhr)+" ,you win!")
             tries += 1
             wins +=1
-    if wins > loses :
-        print("you won the game")
-    elif wins == loses :
-        print("ta3adol")
-    else :
-        print("you lost the game")
+        else :
+            print("\n please chose from hajra/wrqa/mqs \n ")
+            eror +=1
+            break
+    if  int(eror) <= 0 :
+        if wins > loses :
+            print("you won the game")
+        elif wins == loses :
+            print("ta3adol")
+        else :
+            print("you lost the game")
+    else:
+        eror=0
+        tries =0
+        game()
 game()
+            
             
