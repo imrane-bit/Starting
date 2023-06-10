73e4ad30fd6ca681945bab4f32ec08e790671715
imrane-bit <74026154+imrane-bit@users.noreply.github.com>
2023-06-10 18:05:03 +0100

    Update notecalc.py
    massar-like program that calculate a general note based on all subjects coefs and notes

--- notecalc.py
+++ notecalc.py
@@ -10,32 +10,21 @@ class subject :
         self.z = z
         self.coef= coef
 
-math0 = subject("math0","note1","note2","note3",7)
-pc = subject("pc","note1","note2","note3",7)
-svt = subject("svt","note1","note2","note3",7)
-hg = subject("hg","note1","note2","note3",2)
-ei = subject("ei","note1","note2","note3",2)
-fr = subject("fr","note1","note2","note3",4)
-philo = subject("philo","note1","note2","note3",2)
-ar = subject("ar","note1","note2","note3",2)
-en = subject("en","note1","note2","note3",3)
-
-subjects = [math0 , pc , svt ,hg,ei,fr,ar,philo,en]
-coefs =[ coefs.coef for coefs in subjects]
-
-for subject in subjects :
+subjects = ["math" , "pc" , "svt" ,"hg","ei","fr","ar","philo","en"]
+coeuf =[]
+for i in range(9) :
+    subjects[i] = subject(subjects[i],"","","",input(f"{subjects[i]} coefic :"))
+    coeuf.append(float(subjects[i].coef))
+    print(coeuf)
+for i in range(9) :
     try :
-        subject.x=float(input("first note in "+str(subject.name)+": "))
-        subject.y=float(input("second note in "+str(subject.name)+": "))
-        subject.z=float(input("third note in "+str(subject.name)+": "))
+        subjects[i].x=float(input("first note in "+str(subjects[i].name)+": "))
+        subjects[i].y=float(input("second note in "+str(subjects[i].name)+": "))
+        subjects[i].z=float(input("third note in "+str(subjects[i].name)+": "))
     except ValueError as a :
         print(a)
 
-
-total = lambda subject : ((subject.x + subject.y + subject.z)/3)*subject.coef
-
-
-noot = [total(noot) for noot in subjects]
-final = math.fsum(noot)/math.fsum(coefs)
-
+total = lambda i : ((float(subjects[i].x) + float(subjects[i].y) + (subjects[i].z))/3)*float(subjects[i].coef)
+noot = [total(i) for i in range(9)]
+final = math.fsum(noot)/math.fsum(coeuf)
 print ("your average is :"+str(final)) 
